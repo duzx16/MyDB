27d3dcc7176d0ab9f8fbf5b3c4d52bd8afbcdacd
duzx <duzx16@mails.tsinghua.edu.cn>
2018-10-28 16:04:17 +0800

    First build success

---
+++ src/Constants.h
@@ -0,0 +1,13 @@
+//
+// Created by Zhengxiao Du on 2018/10/28.
+//
+
+#ifndef MYDB_CONSTANTS_H
+#define MYDB_CONSTANTS_H
+
+/*
+ * 一个页面中的字节数
+ */
+#define PAGE_SIZE 8192
+
+#endif //MYDB_CONSTANTS_H
