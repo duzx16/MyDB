640e3a6dc8e3daf9b5b57407ff6cb5ec13ef2ffa
duzx <duzx16@mails.tsinghua.edu.cn>
2018-10-29 21:12:25 +0800

    Add the AttrType

--- src/Constants.h
+++ src/Constants.h
@@ -10,5 +10,7 @@
  */
 #define PAGE_SIZE 8192
 typedef unsigned int* BufType;
+enum class AttrType{INT, FLOAT, STRING, NO_ATTR};
+enum class CompOp{EQ_OP, LT_OP, GT_OP, LE_OP, GE_OP, NE_OP, NO_OP};
 
 #endif //MYDB_CONSTANTS_H
