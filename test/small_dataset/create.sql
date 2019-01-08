086b8638037da2a157f4d419bd08651c6b668016
duzx <duzx16@mails.tsinghua.edu.cn>
2019-01-08 10:04:54 +0800

    add primary key in create.sql

--- test/small_dataset/create.sql
+++ test/small_dataset/create.sql
@@ -7,27 +7,31 @@ name varchar(20) not null,
 sex varchar(20),
 age int,
 address varchar(20),
+primary key(sid),
 check(sex in ('man', 'woman')));
+
+create table course (
+cid int,
+subject varchar(20) not null,
+primary key(cid)
+);
+
+create table grade (
+gid int,
+sid int,
+cid int,
+score int not null,
+primary key(gid));
 
 insert into student values (1001, 'Li', 'man', 20, 'sdwf');
 insert into student values (1002, 'Xu', 'man', 23, 'sdgm');
 insert into student values (1003, 'zhao', 'woman', 25, 'sdwf');
 insert into student values (1004, 'Si', 'woman', 24, 'default');
 
-create table course (
-cid int,
-subject varchar(20) not null);
-
 insert into course values (1, 'oracle');
 insert into course values (2, 'English');
 insert into course values (3, 'Java');
 insert into course values (4, 'SQL');
-
-create table grade (
-gid int,
-sid int,
-cid int,
-score int not null);
 
 insert into grade values (101,1001,1,60);
 insert into grade values (102,1001,1,70);
